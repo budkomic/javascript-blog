e9ccba63c079cde6c8e11d43bfa82abea0d38708
Michal Budkowski <michalbudkowski88@gmail.com>
2022-07-07 21:21:44 +0200

    fix js

--- js/script.js
+++ js/script.js
@@ -62,49 +62,4 @@ function titleClickHandler(event){
 function generateTitleLinks(customSelector = ''){
   console.log('Title Lists are just generated');
   
-  /* [DONE] remove contents of titleList */
-  
-  const titleList = document.querySelector(optTitleListSelector);
-  titleList.innerHTML = '';
-  
-  /* for each article */
-  
-  const articles = document.querySelectorAll(optArticleSelector + customSelector);
-  let html = '';
-  for(let article of articles){
-    
-    /* get the article id */
-    const articleId = article.getAttribute('id');
-    console.log(articleId);
-   
-    /* find the title element */
-   
-    /* get the title from the title element */
-   
-    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-   
-    /* create HTML of the link */
-    //const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-   
-    const linkHTMLData = {id: articleId, title: articleTitle};
-	const linkHTML = templates.articleLink(linkHTMLData);
-	console.log('HTML of link was created!');
-    
-    /* insert link into titleList */
-   
-    html = html + linkHTML;
-    console.log(html);
-
-  }
-  
-  titleList.innerHTML = html;
-  
-  const links = document.querySelectorAll('.titles a');
-  console.log('what is it?');
- 
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-    console.log(link);
-  }
-}
 generateTitleLinks();
